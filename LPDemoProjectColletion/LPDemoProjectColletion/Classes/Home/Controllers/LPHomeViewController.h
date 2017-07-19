9e0a099348ffd08b2fc6a9a56246d1f289cb15e8
lizhaopeng <lizhaopeng@lizhaopeng.local>
2017-07-19 11:20:19 +0800

    基础设置
    第一次基础设置

--- LPDemoProjectColletion/LPDemoProjectColletion/Classes/Home/Controllers/LPHomeViewController.h
+++ LPDemoProjectColletion/LPDemoProjectColletion/Classes/Home/Controllers/LPHomeViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  LPHomeViewController.h
 //  LPDemoProjectColletion
 //
 //  Created by lizhaopeng on 2017/7/19.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface LPHomeViewController : UIViewController
 
 @end
-
---
+++ LPDemoProjectColletion/LPDemoProjectColletion/Classes/Home/Controllers/LPHomeViewController.m
@@ -0,0 +1,61 @@
+//
+//  LPHomeViewController.m
+//  LPDemoProjectColletion
+//
+//  Created by lizhaopeng on 2017/7/19.
+//  Copyright © 2017年 LP. All rights reserved.
+//
+
+#import "LPHomeViewController.h"
+
+@interface LPHomeViewController ()
+
+@end
+
+@implementation LPHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    
+    
+    [self buildMainUI];
+    
+}
+
+/**
+ *基础设置
+ */
+- (void)basicSet
+{
+    
+}
+
+- (void)buildMainUI
+{
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:self.view.bounds];
+    
+    imageView.contentMode = UIViewContentModeScaleToFill;
+    
+    imageView.image = [UIImage imageNamed:@"星系"];
+    
+    [self.view addSubview:imageView];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
